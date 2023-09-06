47450c57213a4595bc6eb74300c950f9932a7db3
iamtheuri <theuridavid56@gmail.com>
2023-09-06 23:18:40 +0300

    INITIAL COMMIT

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <h4>Contact Us</h4>
+        <p>
+          Address: 123 Main Street, City, Country
+        </p>
+        <p>
+          Email: info@example.com
+        </p>
+        <p>
+          Phone: +1 (123) 456-7890
+        </p>
+      </div>
+      <div class="col-md-5">
+        <h4>Follow Us</h4>
+        <div class="socials">
+            <a class=""><i class="fa-brands fa-square-facebook fa-shake fa-lg" style="color: #4b511f;"></i></a>
+            <a class=""><i class="fa-brands fa-square-instagram fa-shake fa-lg" style="color: #4b511f;"></i></a>
+            <a class=""><i class="fa-brands fa-linkedin fa-shake fa-lg" style="color: #4b511f;"></i></a>
+            <a class=""><i class="fa-brands fa-twitter fa-shake fa-lg" style="color: #4b511f;"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/photography', function () {
+    return view('photography');
+});
+Route::get('/contacts', function () {
+    return view('contacts');
 });
