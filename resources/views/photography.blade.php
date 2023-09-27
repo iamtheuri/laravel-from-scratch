77d993ec9f594074a1c726f7b6aa802821884b50
iamtheuri <theuridavid56@gmail.com>
2023-09-27 13:11:11 +0300

    Gallery Modal Fixed

--- resources/views/photography.blade.php
+++ resources/views/photography.blade.php
@@ -3,48 +3,39 @@
 @include('partials.header')
 @include('partials.navbar')
 
+<div class="container-fluid wrapper" style="padding-top: 5rem">
 
+    <section class="image-Gallery container">
+        <div class="container-fluid">
+            <h2 class="text-center">Wedding Image Gallery</h2>
+            <p class="welcome-text">Welcome to our captivating gallery of visual stories. Explore the artistry, emotion,
+                and beauty captured through our lens. Each image is a moment frozen in time, a testament to the power of
+                photography. We invite you to immerse yourself in our world of images, where every frame tells a unique
+                story</p>
 
-    <div class="container-fluid wrapper">
+            <div class="gallery container  gallery-container">
 
-    
+                @foreach (range(1, 12) as $galleryNumber)
+                <div class="gallery-image">
+                    <img src="{{ asset('images/home/home_gallery'.$galleryNumber.'.webp') }}"
+                        alt="Gallery Image {{ $galleryNumber }}" class="gallery-image-clickable">
+                </div>
+                @endforeach
 
-<section class="image-Gallery container">
-    <div class="container-fluid">
-        <h2 class="text-center">Wedding Image Gallery</h2>
-        <p class="welcome-text">Welcome to our captivating gallery of visual stories. Explore the artistry, emotion, and beauty captured through our lens. Each image is a moment frozen in time, a testament to the power of photography. We invite you to immerse yourself in our world of images, where every frame tells a unique story</p>
-  
-<div class="gallery container  gallery-container">
+                <!-- The Modal -->
 
+            </div>
+        </div>
 
-@foreach (range(1, 12) as $galleryNumber)
-<div class="gallery-image">
-    <img
-        src="{{ asset('images/home/home_gallery'.$galleryNumber.'.webp') }}"
-        alt="Gallery Image {{ $galleryNumber }}"
-        class="gallery-image-clickable"
-    >
-</div>
-@endforeach
-
-<!-- The Modal -->
-
-
-
-
-
-</div>
-</div>
-
-</section>
-<div id="myModal" class="modal">
-    <span class="close" onclick="closeModal()">&times;</span>
-    <div class="carousel">
-        <img id="modal-content" class="modal-content" src="">
-        <button  id ="prev"class="prev" onclick="prevSlide()">&#10094;</button>
-        <button class="next" onclick="nextSlide()">&#10095;</button>
+    </section>
+    <div id="myModal" class="modal">
+        <div class="carousel">
+            <span class="modal-close" onclick="closeModal()">&times;</span>
+            <img id="modal-content" class="modal-content" src="">
+            <button id="prev" class="prev" onclick="prevSlide()">&#10094;</button>
+            <button class="next" onclick="nextSlide()">&#10095;</button>
+        </div>
     </div>
-</div>
 
 
 </div>
