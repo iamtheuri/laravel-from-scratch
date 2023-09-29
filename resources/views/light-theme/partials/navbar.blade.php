3fb70ba4474f36268e3487e277b63a8b059fc824
iamtheuri <theuridavid56@gmail.com>
2023-09-29 08:55:48 +0300

    NavBar Modification

--- resources/views/light-theme/partials/navbar.blade.php
+++ resources/views/light-theme/partials/navbar.blade.php
@@ -1,9 +1,9 @@
 
- <!-- Spinner Start -->
- <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-grow text-primary" role="status"></div>
-    </div>
-    <!-- Spinner End -->
+<!-- Spinner Start -->
+<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-grow text-primary" role="status"></div>
+</div>
+<!-- Spinner End -->
 
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light fixed-top shadow py-lg-0 px-4 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
@@ -15,17 +15,17 @@
         </button>
         <div class="collapse navbar-collapse justify-content-between py-4 py-lg-0" id="navbarCollapse">
             <div class="navbar-nav ms-auto py-0">
-                <a href="/" class="nav-item nav-link">Home</a>
-                <a href="/about" class="nav-item nav-link">About</a>
+                <a href="/light-theme" class="nav-item nav-link">Home</a>
+                <a href="/light-theme/about" class="nav-item nav-link">About</a>
                 
             </div>
-            <a href="/" class="navbar-brand bg-primary py-2 px-4 mx-3 d-none d-lg-block">
+            <a href="/light-theme" class="navbar-brand bg-primary py-2 px-4 mx-3 d-none d-lg-block">
                 <h1 class="text-white">Queen Dee</h1>
             </a>
             <div class="navbar-nav me-auto py-0">
-                <a href="/photography" class="nav-item nav-link">Gallery</a>
+                <a href="/light-theme/photography" class="nav-item nav-link">Gallery</a>
                
-                <a href="/contact" class="nav-item nav-link">Contact</a>
+                <a href="/light-theme/contact" class="nav-item nav-link">Contact</a>
             </div>
         </div>
     </nav>
